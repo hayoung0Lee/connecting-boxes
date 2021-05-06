4951d349974dbcc9019c46cfa08993597f2ab13b
hayoung0Lee <hayoung0.lee@gmail.com>
2021-05-06 16:56:20 +0900

    flow stroke prototype

--- src/init.js
+++ src/init.js
@@ -4,48 +4,91 @@ let pos1 = 0,
   pos2 = 0,
   pos3 = 0,
   pos4 = 0;
+
+let isConnecting;
+let $container;
+
 window.addEventListener("load", init);
 
+const boxOrBtn = (e) => {
+  if (e.target.classList.contains("box")) {
+    return "box";
+  }
+  if (e.target.classList.contains("btn")) {
+    return "btn";
+  }
+};
+
+const boxDrag = (e) => {
+  target = e.target;
+  isDrawing = true;
+  pos3 = e.clientX;
+  pos4 = e.clientY;
+  isConnecting = false;
+};
+
+const btnDrag = (e) => {
+  if (!isConnecting) {
+    // 새로운 연결 시작
+    target = e.target;
+    isConnecting = true;
+  } else {
+    // 연결 동작 마무리, class명으로 동일한 parent div에 포함인지 확인
+    isConnecting = false;
+    if (e.target.classList.value !== target.classList.value) {
+      console.log("다른 도형끼리 연결!");
+      return;
+    }
+    // 같은 parent div 소속이면 무시
+  }
+};
+
+const mousedown = (e) => {
+  switch (boxOrBtn(e)) {
+    case "box":
+      boxDrag(e);
+      break;
+    case "btn":
+      btnDrag(e);
+      break;
+  }
+};
+
+const mousemove = (e) => {
+  // box일때만 동작한다
+  if (boxOrBtn(e) === "box" && isDrawing === true) {
+    pos1 = pos3 - e.clientX;
+    pos2 = pos4 - e.clientY;
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+
+    target.style.left = target.offsetLeft - pos1 + "px";
+    target.style.top = target.offsetTop - pos2 + "px";
+  }
+
+  if (isConnecting === true) {
+    console.log("연결 중");
+  }
+};
+
+const mouseup = (e) => {
+  // box일때만 동작한다
+  if (boxOrBtn(e) === "box" && isDrawing === true) {
+    isDrawing = false;
+    (pos1 = 0), (pos2 = 0), (pos3 = 0), (pos4 = 0);
+  }
+};
+
 function init() {
-  const $box1 = document.querySelector(".box1");
+  $container = document.querySelector(".container");
+  const $boxes = document.querySelectorAll(".box");
 
-  $box1.addEventListener("mousedown", (e) => {
-    // switch로 바꾸기
-    if (e.target.classList.contains("box")) {
-      target = e.target;
-      isDrawing = true;
-      pos3 = e.clientX;
-      pos4 = e.clientY;
-      return;
-    }
-
-    if (e.target.classList.contains("btn")) {
-      e.stopPropagation();
-      console.log("선 연결");
-      return;
-    }
-  });
-
-  $box1.addEventListener("mousemove", (e) => {
-    if (isDrawing === true) {
-      pos1 = pos3 - e.clientX;
-      pos2 = pos4 - e.clientY;
-      pos3 = e.clientX;
-      pos4 = e.clientY;
-
-      target.style.left = target.offsetLeft - pos1 + "px";
-      target.style.top = target.offsetTop - pos2 + "px";
-    }
-  });
-
-  $box1.addEventListener("mouseup", (e) => {
-    if (isDrawing === true) {
-      isDrawing = false;
-      (pos1 = 0), (pos2 = 0), (pos3 = 0), (pos4 = 0);
-    }
-  });
-}
-
-function dragStart(event) {
-  console.log("start", event, this);
+  for (const $box of $boxes) {
+    // 누르기 시작할때
+    $box.addEventListener("mousedown", mousedown);
+    // 누른 상태에서 이동할때
+    $box.addEventListener("mousemove", mousemove);
+    // 마우스를 뗐을때
+    $box.addEventListener("mouseup", mouseup);
+  }
 }
